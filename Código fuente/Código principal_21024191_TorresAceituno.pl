35a97f6a9bf13aaff7b26963d83fc6feafce1692
Christopher <christopher.torres@usach.cl>
2021-12-15 21:18:45 -0300

    paradigmaDocsRegister
    Se completa y se sigue agregando documentación.

--- Código fuente/Código principal_21024191_TorresAceituno.pl
+++ Código fuente/Código principal_21024191_TorresAceituno.pl
@@ -98,26 +98,143 @@ usuario(Username, Password, Date, User):-
 % Ejemplo: fecha(14, 12, 2021, F1), usuario("Grillitoxc", "123", F1, User1).
 
 
+%-------------------------------------------------------------------------------%
+% Otros predicados (que ayudan al funcionamiento de los predicados principales) %
+%-------------------------------------------------------------------------------%
+
+primero([H|_], H).
+sacarNombres([], []):-!.
+sacarNombres([H|T], [H1|T2]):-
+    primero(H, H1),
+    sacarNombres(T, T2).
+
 %---------------------------------------------------%
 % Código Principal Plataforma que emula Google Docs %
 %---------------------------------------------------%
+% Dominios:
+%   PD1: List (Plataforma ParadigmaDocs).
+%   Username: String.
+%   Password: String.
+%   
+% Predicados:
+%   paradigmaDocsRegister(PD1, Fecha, Username, Password, PD2). aridad = 5
+%
 %
 
-%---paradigmaDocsRegister---%
+
+%-----------------------%
+% paradigmaDocsRegister %
+%-----------------------%
+/*
+Predicado que permite registrar un usuario en la plataforma de paradigmadocs.
+Este predicado no permite Usuarios con el mismo Username, por lo que verifica que no existan usuarios repetidos.
+Además, cada usuario deberá registrarse con una contraseña y una fecha de creación de la cuenta.
+*/
 paradigmaDocsRegister(PD1, Fecha, Username, Password, PD2):-
     selectNombreP(PD1, NombreP),
     selectFechaP(PD1, FechaP),
     selectListaRegP(PD1, ListaReg),
     selectUserActivoP(PD1, UserActivo),
     selectDocumentosP(PD1, ListaDocs),
-    indexOf(ListaReg, )
     usuario(Username, Password, Fecha, User1),
+    sacarNombres(ListaReg, Nombres),
+    \+member(Username, Nombres),
     append(ListaReg, [User1], ListaRegistroNueva),
     PD2 = [NombreP, FechaP, ListaRegistroNueva, UserActivo, ListaDocs].
 
 
-% fecha(20, 12, 2015, D1), fecha(1, 12, 2021, D2), fecha(3, 12, 2021, D3), paradigmaDocs("google docs", D1, PD1), paradigmaDocsRegister(PD1, D2, "vflores", "hola123", PD2), paradigmaDocsRegister(PD2, D2, "crios", "qwert", PD3), paradigmaDocsRegister(PD3, D3, "alopez", "asdfg", PD4).
-% fecha(14, 12, 2021, F1), paradigmaDocs("Plataforma1", F1, Gdocs000), paradigmaDocsRegister(Gdocs000, F1, "Griyitoxc", "123", Gdocs001).
+%--------------------%
+% paradigmaDocsLogin %
+%--------------------%
+/*
+*/
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+%------------------------------%
+% Ejemplos para cada predicado %
+%------------------------------%
+%
+% paradigmaDocsRegister
+%   Se registran 3 usuarios distintos
+%       fecha(20, 12, 2015, D1), fecha(1, 12, 2021, D2), fecha(3, 12, 2021, D3), paradigmaDocs("google docs", D1, PD1), paradigmaDocsRegister(PD1, D2, "vflores", "hola123", PD2), paradigmaDocsRegister(PD2, D2, "crios", "qwert", PD3), paradigmaDocsRegister(PD3, D3, "alopez", "asdfg", PD4).
+%   Se registran 3 usuarios, pero 2 se repiten nombre
+%       fecha(20, 12, 2015, D1), fecha(1, 12, 2021, D2), fecha(3, 12, 2021, D3), paradigmaDocs("google docs", D1, PD1), paradigmaDocsRegister(PD1, D2, "alopez", "hola123", PD2), paradigmaDocsRegister(PD2, D2, "crios", "qwert", PD3), paradigmaDocsRegister(PD3, D3, "alopez", "asdfg", PD4).
+%   Se registra 1 solo usuario
+%       fecha(15, 12, 2021, F1), paradigmaDocs("google docs", F1, PD1), paradigmaDocsRegister(PD1, F1, "Griyitoxc", "paradigmaCLAVE", PD2).
+%
+% paradigmaDocsLogin
+%
+%
+%
+%
+%
+%
+%
+% paradigmaDocsCreate
+%
+%
+%
+%
+%
+%
+%
+% paradigmaDocsShare
+%
+%
+%
+%
+%
+%
+%
+% paradigmaDocsAdd
+%
+%
+%
+%
+%
+%
+%
+% paradigmaDocsRestoreVersion
+%
+%
+%
+%
+%
+%
+%
+% paradigmaDocsToString
+%
+%
+%
+%
+%
+%
+%
