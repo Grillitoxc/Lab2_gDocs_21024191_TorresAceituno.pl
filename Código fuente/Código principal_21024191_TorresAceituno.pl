fc33c2bc5540829d0cca7337dde70a12ec825bd2
Christopher <christopher.torres@usach.cl>
2021-12-17 00:41:46 -0300

    TDA Documento y paradigmaDocsCreate WIP
    Hay un error en paradigmaDocsCreate que cuando se resuelva, se terminará el predicado completo.

--- Código fuente/Código principal_21024191_TorresAceituno.pl
+++ Código fuente/Código principal_21024191_TorresAceituno.pl
@@ -3,7 +3,6 @@
 % Proyecto de laboratorio N°2
 % Plataforma que emula Google Docs
 
-% escribircomando/ ;true. enter w a
 %-----------%
 % TDA Fecha %
 %-----------%
@@ -14,7 +13,7 @@
 %    A: un número entero (representa el año)
 %
 % Predicados
-%    fecha(D, M, A, FOut).                                                  aridad = 4 
+%    fecha(D, M, A, FOut).                                                  aridad = 4
 %
 % Clausulas
 % Hechos
@@ -54,7 +53,6 @@ modificarMesF(F1, M, F2):-
 modificarAnnoF(F1, A, F2):-
     seleccionarDiaF(F1, D1), seleccionarMesF(F1, M1), fecha(D1, M1, A, F2), esFecha(F2).
 
-
 %-------------------%
 % TDA ParadigmaDocs %
 %-------------------%
@@ -70,7 +68,6 @@ modificarAnnoF(F1, A, F2):-
 % Constructor/Pertinencia/Modificadores
 paradigmaDocs(Name, Date, ParadigmaDocs):-
     string(Name), esFecha(Date), ParadigmaDocs = [Name, Date, [], [], []].
-% Ejemplo: fecha(14, 12, 2021, F1), paradigmaDocs("Plataforma1", F1, Gdocs000).
 
 % Selectores
 selectNombreP([NombreP, _, _, _, _], NombreP).
@@ -78,6 +75,15 @@ selectFechaP([_, FechaP, _, _, _], FechaP).
 selectListaRegP([_, _, ListaReg, _, _], ListaReg).
 selectUserActivoP([_, _, _, UserActivo, _], UserActivo).
 selectDocumentosP([_, _, _, _, ListaDocs], ListaDocs).
+selectNombreUser([User], User).
+
+% Modificadores 
+setListaVacia(PD1, PD2):-
+    selectNombreP(PD1, Nombre1),
+    selectFechaP(PD1, Fecha1),
+    selectListaRegP(PD1, Registro1),
+    selectDocumentosP(PD1, Documento1),
+    PD2 = [Nombre1, Fecha1, Registro1,[], Documento1].
 
 %-------------%
 % TDA Usuario %
@@ -95,14 +101,41 @@ selectDocumentosP([_, _, _, _, ListaDocs], ListaDocs).
 % Constructor/Pertinencia/Selector/Modificadores
 usuario(Username, Password, Date, User):-
     string(Username), string(Password), esFecha(Date), User = [Username, Password, Date].
-% Ejemplo: fecha(14, 12, 2021, F1), usuario("Grillitoxc", "123", F1, User1).
 
+%---------------%
+% TDA Documento %
+%---------------%
+%
+% Dominios
+%    Nombre: String (Nombre del documento)
+%    Fecha: Fecha (Fecha de creación)
+%
+% Predicados
+%    documento(Nombre, Fecha, Contenido, DOut)                              aridad = 4
+%
+% Clausulas
+% Constructor/Pertinencia/Selector
+documento(Nombre, Fecha, Contenido, Doc):-
+    string(Nombre), esFecha(Fecha), string(Contenido),
+    Doc = [Nombre, Fecha, "", [[Contenido, Fecha, 0]], []].
+
+% Selectores
+selectNombreD([NombreD, _, _, _, _], NombreD).
+selectFechaD([_, FechaD, _, _, _], FechaD).
+selectIdD([_, _, IdD, _, _], IdD).
+selectVersiones([_, _, _, ListaVer, _], ListaVer).
+selectAccesos([_, _, _, _, ListaAccesos], ListaAccesos).
+
+% Modificadores
+setIdDoc(Doc1, Id, Doc2):-
+    selectNombreD(Doc1, NombreDoc), selectFechaD(Doc1, FechaDoc), selectVersiones(Doc1, VerDoc), selectAccesos(Doc1, AccDoc),
+    Doc2 = [NombreDoc, FechaDoc, Id, VerDoc, AccDoc].
 
 %-------------------------------------------------------------------------------%
 % Otros predicados (que ayudan al funcionamiento de los predicados principales) %
 %-------------------------------------------------------------------------------%
 miembro(X, [X|_]):-!.
-    miembro(X, [_|T]) :- miembro(X, T).
+    miembro(X, [_|T]):-miembro(X, T).
 
 primero([H|_], H).
 sacarNombres([], []):-!.
@@ -124,11 +157,14 @@ sacarNombresAndContrasenas([H|T], [H1|T2]):-
 %   Username: String.
 %   Password: String.
 %   
+%   
 % Predicados:
 %   paradigmaDocsRegister(PD1, Fecha, Username, Password, PD2).             aridad = 5
 %   paradigmaDocsLogin(PD1, Username, Password, PD2).                       aridad = 4
 %
-
+% Metas:
+%   Principales: paradigmaDocsRegister, paradigmaDocsLogin
+%   Secundarias: selectNombreP, selectFechaP
 
 %-----------------------%
 % paradigmaDocsRegister %
@@ -171,6 +207,20 @@ paradigmaDocsLogin(PD1, Username, Password, PD2):-
     PD2 = [NombreP, FechaP, ListaReg, [Username], ListaDocs].
 
 
+%---------------------%
+% paradigmaDocsCreate %
+%---------------------%
+/*
+Predicado que 
+*/
+paradigmaDocsCreate(PD1, Fecha, Nombre, Contenido, PD2):-
+    selectUserActivoP(PD1, UserActivo),
+    \+UserActivo==[],
+    selectNombreUser(UserActivo, NombreUserActivo),
+    documento(Nombre, Fecha, Contenido, DocumentoCreado),
+    selectDocumentosP(PD1, ListaDocs),
+    length(ListaDocs, NumId),
+    setIdDoc(DocumentoCreado, NumId, DocumentoActualizado).
 
 
 
@@ -209,6 +259,8 @@ paradigmaDocsLogin(PD1, Username, Password, PD2):-
 %       fecha(20, 12, 2015, D1), fecha(1, 12, 2021, D2), fecha(3, 12, 2021, D3), paradigmaDocs("google docs", D1, PD1), paradigmaDocsRegister(PD1, D2, "alopez", "hola123", PD2), paradigmaDocsRegister(PD2, D2, "crios", "qwert", PD3), paradigmaDocsRegister(PD3, D3, "alopez", "asdfg", PD4).
 %   Se registra 1 solo usuario
 %       fecha(15, 12, 2021, F1), paradigmaDocs("google docs", F1, PD1), paradigmaDocsRegister(PD1, F1, "Griyitoxc", "paradigmaCLAVE", PD2).
+%   UNIFICACIÓN del ultimo ejemplo
+%       fecha(15, 12, 2021, [15, 12, 2021]), paradigmaDocs("google docs", [15, 12, 2021], ["google docs", [15, 12, 2021], [], [], []]), paradigmaDocsRegister(["google docs", [15, 12, 2021], [], [], []], [15, 12, 2021], "Griyitoxc", "paradigmaCLAVE", ["google docs", [15, 12, 2021], [["Griyitoxc", "paradigmaCLAVE", [15, 12, 2021]]], [], []]).
 %
 % paradigmaDocsLogin
 %   Se Logea el usuario "vflores" con la contraseña "hola123"
